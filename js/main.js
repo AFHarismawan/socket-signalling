e9a4ea419322af3dc3be4ac27da12386815cfdd3
harismawan <achmadfauziharismawan@gmail.com>
2018-05-17 00:38:10 +0700

    test

--- js/main.js
+++ js/main.js
@@ -229,7 +229,7 @@ function requestTurn(turnURL) {
 
         for (var i = 1; i < turnServer.v.iceServers.length; i++) {
           pcConfig.iceServers.push({
-            'url': turnServer.v.iceServers[i].username + '@' + turnServer.v.iceServers[i].url,
+            'url': 'turn:' + turnServer.v.iceServers[i].username + '@' + turnServer.v.iceServers[i].url.substr(5),
             'credential': turnServer.v.iceServers[i].credential
           });
           console.log(turnServer.v.iceServers[i].username + '@' + turnServer.v.iceServers[i].url + turnServer.v.iceServers[i].credential);
